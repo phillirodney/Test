d184c3750c4c78a807edd5045e3021d1ce4010ba
PHILLICHILLI <PHILLICHILLI>
2017-02-07 15:23:24 +0000

    restart eclipse!

--- src/Zoo.java
+++ src/Zoo.java
@@ -2,7 +2,12 @@
 public class Zoo {
 
 	public static void main(String[] args) {
-
+		
+		for(int i=0 ; i < 5 ; i++)
+	    {
+	            System.out.println("i is : " + i);
+	    }
+		//restart
 		System.out.print("HelloWorld");
 	}
 
