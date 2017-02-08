aedc7f5896de969f1cc794478eb777efee605e51
PHILLICHILLI <PHILLICHILLI>
2017-02-08 15:20:53 +0000

    mammal implements Run and inherits unimplemented method sprint

--- src/com/philli/zoopackage/main/java/Mammal.java
+++ src/com/philli/zoopackage/main/java/Mammal.java
@@ -1,6 +1,6 @@
 package com.philli.zoopackage.main.java;
 
-public class Mammal extends Animal {
+public class Mammal extends Animal implements Run {
 
 	public Mammal(int age, String name, String species, String breed, int weight, int height, EnclosureType enclosure) {
 		super(age, name, species, breed, weight, height, enclosure);
@@ -8,12 +8,17 @@ public class Mammal extends Animal {
 
 	@Override
 	public void reproduction() {
-//reproduction method
+		// reproduction method
 	}
 
 	@Override
 
 	public void growth() {
-//growth method
+		// growth method
+	}
+
+	@Override
+	public void sprint() {
+		// overridden run method, inherited from the Run interface
 	}
 }
