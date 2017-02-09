65bab1fe7b1b2887914a2813a58aa1702edc75d3
PHILLICHILLI <PHILLICHILLI>
2017-02-09 11:24:01 +0000

    invertibrate class added as abstract

---
+++ src/com/philli/zoopackage/main/java/Invertibrate.java
@@ -0,0 +1,9 @@
+package com.philli.zoopackage.main.java;
+
+abstract class Invertibrate extends Animal {
+
+	public Invertibrate(int age, String name, String species, int weight, int height, EnclosureType enclosure, boolean endangered, boolean nocturnal, String gender, int hoursAfterEating) {
+		super(age, name, species, weight, height,enclosure, endangered, nocturnal, gender, hoursAfterEating);
+	}
+
+}
